c4c31657659952019cd0074c4155c5cfea196215
Jacob MacDonald <jaccarmac@gmail.com>
2018-09-19 22:25:32 -0500

    The rest of the process.

--- dotnet-core-build.sh
+++ dotnet-core-build.sh
@@ -2,3 +2,6 @@ dotnet build -c Release Kl.sln
 dotnet test -c Release Kl.Tests
 dotnet run -c Release -p Kl.Get
 dotnet run -c Release -p Kl.Make
+dotnet build -c Release Shen.sln
+dotnet test -c Release Shen.Tests
+dotnet run -c Release Shen.TestSuite
